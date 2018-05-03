e5b3021d0e371e15de8f3cdb8008f28d686984a2
vallery <vallerylancey@gmail.com>
2018-05-03 10:41:04 +0200

    Switched SQL to WHERE person_id IN

--- main.py
+++ main.py
@@ -5,13 +5,7 @@ import sys
 
 
 def build_query(ids):
-    sql_base = "SELECT * FROM people WHERE "
-
-    sql_where = []
-    for _ in ids:
-        sql_where.append("person_id = %s")
-
-    sql = sql_base + " OR ".join(sql_where)
+    sql = "SELECT * FROM people WHERE person_id IN (%s)" % ", ".join(ids)
 
     results = database.select(sql, ids)
     print(results)
